a82aa99be180c2a60074f076e404799117141592
Michael Termer <termer@protonmail.com>
2018-07-29 00:34:33 -0400

    fix javadoc for handle

--- src/main/java/net/termer/twister/handler/RouteHandler.java
+++ src/main/java/net/termer/twister/handler/RouteHandler.java
@@ -8,8 +8,7 @@ package net.termer.twister.handler;
 public interface RouteHandler {
 	/**
 	 * The method to return content for a route
-	 * @param req the Request object for the request
-	 * @param res the Response object for the request
+	 * @param wildcards the text filled in for the wildcards in the route this is handling
 	 * @return content for the response
 	 * @since 0.3
 	 */

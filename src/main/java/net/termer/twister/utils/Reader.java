9d50c872556f677a811055ea58f5026bf8e2a6e8
Michael Termer <termer@protonmail.com>
2018-07-29 00:12:30 -0400

    remove unused import

--- src/main/java/net/termer/twister/utils/Reader.java
+++ src/main/java/net/termer/twister/utils/Reader.java
@@ -1,7 +1,6 @@
 package net.termer.twister.utils;
 
 import java.io.BufferedReader;
-import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
